211cf35c491b022bfe310ba67e11e74a266e0eda
Adel Ali <adelnobel@gmail.com>
2014-11-09 16:18:43 +0200

    Updated for Fifa15.
    URLS were updated for Fifa15. 
    Some functions were added.
    1-Relisting

--- Fifa14Client/WebAppFunctioner.py
+++ Fifa14Client/WebAppFunctioner.py
@@ -1,30 +1,41 @@
 from Fifa14Client import Card
 import requests
+import urlparse
+import urllib
 from Fifa14Client.Exceptions import BadRequestException,FUTErrorCodeException
+import sys, traceback
+from time import sleep
 
 class WebAppFunctioner(object):
     """
      This class does what the webapp generally does.
      I am slowly adding more things to it, hopefully it reaches feature parity to the webapp.
     """
-    COIN_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/user/credits'
-    TRANSFER_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/transfermarket?type=%s&lev=%s' \
-                   '&pos=%s&num=%s&team=%s&macr=%s&micr=%s&minb=%s&nat=%s&maxb=%s' \
+    COIN_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/user/credits'
+    TRANSFER_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/transfermarket?type=%s&lev=%s' \
+                   '&pos=%s&num=%s&team=%s&macr=%s&micr=%s&minb=%s&maxb=%s' \
                    '&playStyle=%s&leaga=%s&start=%s&cat=%s&definitionId=%s&maskedDefId=%s'
 
-    BID_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/trade/%s/bid'
-    MOVE_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/item'
-    UNASSIGNED_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/purchased/items'
-    LIST_CARD_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/auctionhouse'
-    TRADEPILE_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/tradepile'
-    QUICKSELL_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/item/%s'
-    WATCHLIST_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/watchlist'
-    WATCHLIST_REMOVE_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/watchlist?tradeId=%s'
-    TRADEPILE_REMOVE_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/trade/%s'
-    SQUAD_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/squad/%s'
-    CLUB_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/club?count=%s&level=%s&type=%s&start=%s'
-    ACCOUNT_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/user'
-    LEADER_URL = 'https://utas.%sfut.ea.com/ut/game/fifa14/leaderboards/period/alltime/user/%s'
+    BID_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/trade/%s/bid'
+    MOVE_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/item'
+    UNASSIGNED_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/purchased/items'
+    LIST_CARD_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/auctionhouse'
+    TRADEPILE_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/tradepile'
+    SEND_TO_TRANSFER_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/item/resource'
+    QUICKSELL_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/item/%s'
+    WATCHLIST_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/watchlist'
+    WATCHLIST_REMOVE_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/watchlist?tradeId=%s'
+    TRADEPILE_REMOVE_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/trade/%s'
+    CLUB_PILES_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/club/consumables/%s'
+    SQUAD_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/squad/%s'
+    CLUB_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/club?count=%s&level=%s&type=%s&start=%s'
+    ACCOUNT_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/user'
+    LEADER_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/leaderboards/period/alltime/user/%s'
+    RELIST_URL = 'https://utas.%sfut.ea.com/ut/game/fifa15/auctionhouse/relist'
+
+
+    def rest(self):
+        sleep(1)
 
     def __init__(self, login_manager):
         self.login_manager = login_manager
@@ -32,6 +43,8 @@ class WebAppFunctioner(object):
             self.platform_string = 's2.'
         else:
             self.platform_string = ''
+
+    
 
     def get_headers(self, http_method):
         """ Returns the headers yo be used in the request
@@ -46,6 +59,7 @@ class WebAppFunctioner(object):
            Raises Exception if unavailable to get coins
         """
         r = requests.post(self.COIN_URL % self.platform_string, headers=self.get_headers('GET'))
+        self.rest()
         try:
             json = r.json()
         except ValueError:
@@ -56,9 +70,11 @@ class WebAppFunctioner(object):
             raise FUTErrorCodeException('Error retrieving coins\n',json)
         else:
             raise BadRequestException('Can not retrieve coins.')
+        
+        
 
-    def search(self, type="", lev="", pos="", num=10, team="",
-               macr="", micr="", minb="", nat="", maxb="",
+    def search(self, type="", lev="gold", pos="", num=10, team="",
+               macr="", micr="", minb="", maxb="",
                playStyle="", leag="", start=0, cat="",
                definitionId="", maskedDefId=""):
         """ Returns a list of Card objects from the search paramters given
@@ -80,9 +96,18 @@ class WebAppFunctioner(object):
             definitionId - you pass the assetId to it and it responds with only those types of cards
             maskeddefId - not certain but seems to do the same as definitionId
         """
-        the_url = self.TRANSFER_URL % (self.platform_string,type, lev, pos, num, team, macr, micr, minb, nat, maxb,
+        the_url = self.TRANSFER_URL % (self.platform_string,type, lev, pos, num, team, macr, micr, minb, maxb,
                                        playStyle, leag, start, cat, definitionId, maskedDefId)
+        parsed = urlparse.urlparse(the_url)
+        pars = {}
+        for (key, value) in urlparse.parse_qsl(parsed.query):
+            if len(value) != 0:
+                pars[key] = value
+        #print pars
+        the_url = parsed.scheme + "://" + parsed.netloc + parsed.path + "?" +  urllib.urlencode(pars)
+        #print the_url
         r = requests.post(the_url, headers=self.get_headers('GET'))
+        self.rest()
         try:
             json = r.json()
         except:
@@ -98,30 +123,45 @@ class WebAppFunctioner(object):
         payload = '{"bid":%s}' % (price)
         the_url = self.BID_URL % (self.platform_string, card.tradeId)
         r = requests.post(the_url, headers=self.get_headers('PUT'), data=payload)
+        self.rest()
         #There will be no code key in the json if the bid is sucessful
         try:
             json = r.json()
         except:
-            raise BadRequestException("Could not bid. No JSON Object could be decoded.")
+            #raise BadRequestException("Could not bid. No JSON Object could be decoded.")
+            return False
         if 'code' in json:
-            raise FUTErrorCodeException("Could not place bid.",json)
-
+            #raise FUTErrorCodeException("Could not place bid.",json)
+            return False
+        return True
 
     def move(self, card, pile):
         """Moves card to specified pile, raises an exception on failure"""
         payload = '{"itemData":[{"id":"%s","pile":"%s"}]}' % (card.id, pile)
         r = requests.post(self.MOVE_URL % self.platform_string, headers=self.get_headers('PUT'), data=payload)
+        self.rest()
         #There will be no code key in the json if the move is sucessful
         try:
             json = r.json()
+            print json
+            if json["itemData"][0]["success"] == False:
+                print "False 1"
+                return False
         except:
-            raise BadRequestException("Could not move card to %s. No JSON Object could be decoded." % pile)
+            #raise BadRequestException("Could not move card to %s. No JSON Object could be decoded." % pile)
+            traceback.print_exc()
+            print "False 2"
+            return False
         if 'code' in json:
-            raise FUTErrorCodeException("Could not move card to %s" % pile,json)
+            #raise FUTErrorCodeException("Could not move card to %s" % pile,json)
+            print "False 3"
+            return False
+        return True
 
     def get_unassigned_pile(self):
         """Returns a list of Card objects from the unnasigned pile,raises exception on failure."""
         r = requests.post(self.UNASSIGNED_URL % self.platform_string, headers=self.get_headers('GET'))
+        self.rest()
         try:
             json = r.json()
         except:
@@ -138,22 +178,43 @@ class WebAppFunctioner(object):
             Returns the tradeId of the card that was just listed
             Raises an exception on failure
         """
-        payload = '{"buyNowPrice":%s,"itemData":{"id":%s},"duration":%s,"startingBid":%s}' % \
-                  (buy_now_price, card.id, duration, starting_bid)
+        payload = '{"itemData":{"id":%s}, "buyNowPrice":%s, "startingBid":%s, "duration":%s}' % \
+                  (card.id, buy_now_price, starting_bid, duration)
         r = requests.post(self.LIST_CARD_URL % self.platform_string, headers=self.get_headers('POST'), data=payload)
+        self.rest()
+        #print self.LIST_CARD_URL % self.platform_string, payload
         #There will be no code key in the json if the listing is sucessful
         try:
             json = r.json()
+            #print json
         except:
-            raise BadRequestException("Could not list card to tradepile. No JSON Object could be decoded.")
+            return False
+            #raise BadRequestException("Could not list card to tradepile. No JSON Object could be decoded.")
         if 'code' in json:
-            raise FUTErrorCodeException("Could not list card in tradepile.",json)
+            return False
+            #raise FUTErrorCodeException("Could not list card in tradepile.",json)
         else:
-            return json['id']
+            return True
+
+    def relist_all(self):
+        """Relists all cards!"""
+        r = requests.post(self.RELIST_URL % self.platform_string, headers=self.get_headers('PUT'))
+        self.rest()
+        try:
+            json = r.json()
+            print json
+        except:
+            raise BadRequestException("Could not get unassigned pile. No JSON object could be decoded")
+            #return False
+        if 'code' in json:
+            raise FUTErrorCodeException("Could not get unassigned pile.",json)
+            #return False
+        #return True
 
     def get_tradepile(self):
         """Returns a list of Card objects from the tradepile, raises an exception on failure"""
         r = requests.post(self.TRADEPILE_URL % self.platform_string, headers=self.get_headers('GET'))
+        self.rest()
         try:
             json = r.json()
         except:
@@ -168,16 +229,20 @@ class WebAppFunctioner(object):
         """Quicksells specified card, raises an exception on failure"""
         the_url = self.QUICKSELL_URL % (self.platform_string,card.id)
         r = requests.post(the_url, headers=self.get_headers('DELETE'))
+        self.rest()
         try:
             json = r.json()
         except:
-            raise BadRequestException("Could not quicksell card. No JSON Object could be decoded.")
+            return False
+            #raise BadRequestException("Could not quicksell card. No JSON Object could be decoded.")
         if 'code' in json:
-            raise FUTErrorCodeException("Could not quicksell card",json)
-
+            return False
+            #raise FUTErrorCodeException("Could not quicksell card",json)
+        return True
     def get_watchlist(self):
         """Returns a list of Card objects from the watchlist, raises an exception on failure."""
         r = requests.post(self.WATCHLIST_URL % self.platform_string, headers=self.get_headers('GET'))
+        self.rest()
         try:
             json = r.json()
         except:
@@ -188,14 +253,53 @@ class WebAppFunctioner(object):
         elif 'code' in json:
             raise FUTErrorCodeException("Could not get watchlist.",json)
 
+    def get_club_pile(self, pile):
+        """Returns a list of Card objects from the training pile in consumabled, raises an exception on failure."""
+        r = requests.post(self.CLUB_PILES_URL % (self.platform_string, pile), headers=self.get_headers('GET'))
+        self.rest()
+        try:
+            json = r.json()
+        except:
+            raise BadRequestException("Could not get watchlist. No JSON object could be decoded")
+        if 'itemData' in json:
+            card_list = json['itemData']
+            #print card_list
+            return [card_dict for card_dict in card_list]
+        elif 'code' in json:
+            raise FUTErrorCodeException("Could not get training pile.",json)
+
+    def move_to_transfer_list(self, resourceId):
+        """Moves card to specified pile, raises an exception on failure"""
+        payload = '{"itemData":[{"id":"%s","pile":"trade"}]}' % (resourceId)
+        r = requests.post(self.SEND_TO_TRANSFER_URL % self.platform_string, headers=self.get_headers('PUT'), data=payload)
+        self.rest()
+        #There will be no code key in the json if the move is sucessful
+        try:
+            json = r.json()
+            #print json
+            if json["itemData"][0]["success"] == False:
+                print "False 1"
+                return False
+        except:
+            #raise BadRequestException("Could not move card to %s. No JSON Object could be decoded." % pile)
+            traceback.print_exc()
+            print "False 2"
+            return False
+        if 'code' in json:
+            #raise FUTErrorCodeException("Could not move card to %s" % pile,json)
+            print "False 3"
+            return False
+        return True
+
     def remove_card_from_watchlist(self, card):
         """Removes card from watchlist, raises an exception on failure."""
         the_url = self.WATCHLIST_REMOVE_URL % (self.platform_string, card.tradeId)
         r = requests.post(the_url, headers=self.get_headers('DELETE'))
+        self.rest()
         #The response will be blank on success and will return json saying the reason it failed
         try:
             json = r.json()
-            raise FUTErrorCodeException("Could not remove card from watchlist",json)
+            #raise FUTErrorCodeException("Could not remove card from watchlist",json)
         except:
             pass
 
@@ -203,6 +307,7 @@ class WebAppFunctioner(object):
         """Removes card from tradepile, raises an exception if it fails"""
         the_url = self.TRADEPILE_REMOVE_URL % (self.platform_string, card.tradeId)
         r = requests.post(the_url, headers=self.get_headers('DELETE'))
+        self.rest()
         try:
             json = r.json()
             raise FUTErrorCodeException("Could not remove card from watchlist",json)
@@ -214,6 +319,7 @@ class WebAppFunctioner(object):
         """
         the_url = self.SQUAD_URL % (self.platform_string,squad_num)
         r = requests.post(the_url,headers=self.get_headers('GET'))
+        self.rest()
         try:
             json = r.json()
             if 'code' in json:
@@ -236,6 +342,7 @@ class WebAppFunctioner(object):
         """
         the_url = self.CLUB_URL % (self.platform_string,count,level,type,start)
         r = requests.post(the_url, headers=self.get_headers('GET'))
+        self.rest()
         try:
             json = r.json()
             if 'code' in json:
@@ -249,6 +356,7 @@ class WebAppFunctioner(object):
         '''Returns user id of current account'''
         the_url = self.ACCOUNT_URL % self.platform_string
         r = requests.post(the_url,headers=self.get_headers('GET'))
+        self.rest()
         try:
             json = r.json()
             if 'code' in json:
@@ -262,9 +370,9 @@ class WebAppFunctioner(object):
         """Returns desired leaderboard stats for user"""
         userId = self.get_user_id()
         the_url = self.LEADER_URL % (self.platform_string, userId)
-        print the_url
 
         r =  requests.post(the_url,headers=self.get_headers('GET'))
+        self.rest()
                
         sIndex = {'earnings': 0, #competitor
         'transfer': 1, #trader
